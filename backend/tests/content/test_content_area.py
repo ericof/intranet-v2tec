d0b8b183ef80d855bb01c310db334560c1028482
Érico Andrei <ericof@simplesconsultoria.com.br>
2026-06-09 14:37:40 -0300

    Exercício 41.1: Cria grupo de usuáios ao criarmos uma nova área

--- backend/tests/content/test_content_area.py
+++ backend/tests/content/test_content_area.py
@@ -107,6 +107,21 @@ class TestArea:
             area = api.content.create(container=container, **payload)
         assert area.exclude_from_nav is True
 
+    def test_subscriber_added_create_group(self, area_payload):
+        container = self.portal
+        with api.env.adopt_roles(["Manager"]):
+            area = api.content.create(
+                container=container,
+                **area_payload,
+            )
+        uid = api.content.get_uuid(area)
+        g_id = f"{uid}-editores"
+        grupo = api.group.get(g_id)
+        assert grupo is not None
+        assert grupo.getProperty("title") == f"Área {area.title}: Editores"
+        local_roles = api.group.get_roles(group=grupo, obj=area)
+        assert "Editor" in local_roles
+
 
 class TestAreaFunctional:
     @pytest.fixture(autouse=True)
